3a534079c0f10aebbc6e0fe90bbdd377b8ca2662
Clouds Flowing <clouds.flowing@gmail.com>
2026-06-21 18:08:13 +0800

    add get_response_log_probs

--- cs336_alignment/inference/data.py
+++ cs336_alignment/inference/data.py
@@ -1,4 +1,4 @@
-from transformers import PreTrainedTokenizer
+from transformers import PreTrainedTokenizer, PreTrainedModel
 from dataclasses import dataclass
 import torch
 
@@ -14,21 +14,23 @@ def tokenize_prompt_and_output(
   tokenizer: PreTrainedTokenizer,
 ) -> TokenizedPromptAndOutput:
   # input_strs = [f"{a}{b}" for (a, b) in zip(prompt_strs, output_strs)]
-  prompt_ids = tokenizer(
+  prompt_result = tokenizer(
     prompt_strs,
     padding=True,
     truncation=True,
     return_tensors="pt",
-  ).input_ids
-  output_ids = tokenizer(
+  )
+  prompt_ids = prompt_result.input_ids
+  prompt_lengths = prompt_result.attention_mask.sum(dim=1)
+  output_result = tokenizer(
     output_strs,
     padding=True,
     truncation=True,
     return_tensors="pt",
-  ).input_ids
+  )
+  output_ids = output_result.input_ids
+  output_lengths = output_result.attention_mask.sum(dim=1)
 
-  prompt_lengths = (prompt_ids != tokenizer.pad_token_id).sum(dim=1)
-  output_lengths = (output_ids != tokenizer.pad_token_id).sum(dim=1)
   max_length = (prompt_lengths + output_lengths).max()
   tokenized_ids = torch.full((len(prompt_strs), max_length), tokenizer.pad_token_id)
   mask = torch.zeros_like(tokenized_ids, dtype=torch.bool)
@@ -41,3 +43,31 @@ def tokenize_prompt_and_output(
   mask = mask[:, 1:]
 
   return TokenizedPromptAndOutput(input_ids=input_ids, labels=labels, response_mask=mask)
+
+
+@dataclass
+class ResponseLogProbs:
+  log_probs: torch.Tensor
+  token_entropy: torch.Tensor | None = None
+
+def get_response_log_probs(
+  model: PreTrainedModel,
+  input_ids: torch.Tensor,
+  labels: torch.Tensor,
+  return_token_entropy: bool = False,
+) -> ResponseLogProbs:
+  with torch.no_grad():
+    outputs = model(input_ids=input_ids, labels=labels)
+    output_logits = outputs.logits
+    # print(output_logits.shape, labels.shape)
+    log_probs = -torch.nn.functional.cross_entropy(
+      output_logits.view(-1, output_logits.size(-1)),
+      labels.view(-1),
+      reduction="none"
+    ).view(labels.size())
+    # print(log_probs.shape)
+    token_entropy = None
+    if return_token_entropy:
+      probs = torch.nn.functional.softmax(outputs.logits, dim=-1)
+      token_entropy = -torch.sum(probs * torch.log(probs + 1e-8), dim=-1)
+    return ResponseLogProbs(log_probs=log_probs, token_entropy=token_entropy)
